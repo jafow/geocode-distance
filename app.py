05184d0b4691d3945ee997c70c8d8e3c3a5a77f5
jafow <jared.a.fowler@gmail.com>
2018-03-17 15:57:12 -0700

    refactor to tornado

--- app.py
+++ app.py
@@ -1,47 +1,35 @@
-from flask import Flask
-from flask import render_template, request
-import requests as req
-
-f = open('secrets.txt').read().strip('\n')
-
-API_TOKEN = f
-
-GEOCODE_URL = 'https://maps.googleapis.com/maps/api/geocode/json?'
-
-app = Flask(__name__)
+import tornado.ioloop
+import tornado.web
 
 
-@app.route('/')
-def index():
-    return 'home'
+class MainController(tornado.web.RequestHandler):
+    def get(self):
+        return 'main'
 
 
-@app.route('/geocode', methods=['GET', 'POST'])
-def geocode():
-    street = request.args.get('street')
-    # street = street.replace(' ', '+')
-    city = request.args.get('city')
-
-    print('street: ', street, ' city', city)
-
-    url = '{0}address={1}{2}&key={3}'.format(
-            GEOCODE_URL,
-            street,
-            city,
-            API_TOKEN)
-
-    r = req.get(url)
-
-    if r.status_code >= 200 and r.status_code < 400:
-        res = r.json()
-        return render_template('geo.html', street=street, city=city, rest=res)
-    else:
-        return render_template(
-                'error.html',
-                error='There was an error fetching the address')
+class GeoController(tornado.web.RequestHandler):
+    def get(self):
+        return 'Geo'
 
 
-# geocode api is at:
-# https://maps.googleapis.com/maps/api/geocode/json
-# geocode lookup is at /?address=<1000+Street+Name+Blvd, +City, CA>&key=<TOKEN>
-# reverse lat/long is at /?latlng=40.714224,-73.961452&key=<TOKEN>
+class ReverseController(tornado.web.RequestHandler):
+    def get(self):
+        return 'Reverse'
+
+
+class DistanceController(tornado.web.RequestHandler):
+    def get(self):
+        return 'distance'
+
+
+def main():
+    return tornado.web.Application([
+        (r'/', MainController),
+        (r'/geocode', GeoController),
+        (r'/reverse', ReverseController),
+        ('/distance', DistanceController)
+        ])
+
+
+if __name__ == '__main__':
+    app = main()
