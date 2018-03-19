568c943ff1d6f40d75605eb291e019f235c9507b
jafow <jared.a.fowler@gmail.com>
2018-03-18 19:02:15 -0700

    wip: format query params and setup unittesting

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
-from tornado import ioloop, web, httpclient, gen
+import unittest
+from tornado import ioloop, web, httpclient
 
 settings = {
     "static_path": os.path.join(os.path.dirname(__file__), "static"),
@@ -10,6 +11,7 @@ settings = {
     "template_path":  os.path.join(os.path.dirname(__file__), "views"),
     "compiled_template_cache": False
 }
+
 API_TOKEN = open('secrets.txt').read().strip('\n')
 GEOCODE_URL = '//maps.googleapis.com/maps/api/geocode/json?'
 ADDRESS_URL = '//maps.googleapis.com/maps/api/address/json?'
@@ -24,8 +26,14 @@ class GeoController(web.RequestHandler):
     async def post(self):
         street = self.get_argument('street')
         city = self.get_argument('city')
-        state = self.get_argument('state')
-        _st = street.replace(' ', '+')
+        if not street or not city:
+            # fails fast if missing args
+            return self.render(
+                    'error.html',
+                    msg='Missing required "City" & "Street" values'
+                    )
+
+        geocode_params = format_params(dict(street=street, city=city))
 
         def handle_geo_response(payload):
             ''' callback invoked on the response from geocode API '''
@@ -33,6 +41,14 @@ class GeoController(web.RequestHandler):
                 self.render('error.html', msg=payload.error)
             else:
                 self.render('geo-results.html', street='', body=payload.body)
+
+        def format_params(params) -> dict:
+            ''' escape, format, validate each string in a list of params '''
+            res = dict()
+            for key, val in params:
+                if not val:
+                    self.render('error.html', msg='Missing required field:{}'.format(key))
+                res[key] = val.replace(' ', '+')
 
         request = httpclient.AsyncHTTPClient()
         url = 'https:{0}address={1},+{2}+{3}&key={4}'.format(
---
+++ test.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class TestGeoControllerHandler(unittest.TestCase):
+    def test_format_params(self):
+        self.assertEqual(1, 1, 'test')
+
+
+if __name__ == '__main__':
+    unittest.main()
